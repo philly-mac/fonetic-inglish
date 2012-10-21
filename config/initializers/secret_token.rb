88e5bcb486b3eb6bf0470a38086b2005c65d49fc
Philip MacIver <philip@ivercore.com>
2012-10-21 10:51:33 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoneticEnglish::Application.config.secret_token = '8485df1fd80bebd423acabe1ff32a12f74192d8074c75cb089a9e819651d84ee573d2d8f3b71751dfa2844ca957230f1ef4baa8806d6c06b69e208fbeab3bf59'
